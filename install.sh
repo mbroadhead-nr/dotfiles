962473a3f2352f907905077409e45a916458b9b9
Matthew Broadhead <matthew.broadhead@netreveal.ai>
2023-10-18 17:20:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-code --install-extension github.copilot
+# Find the path to the code executable
+VSCODE_EXECUTABLE=$(which code)
+
+# Check if the code executable is found
+if [ -z "$VSCODE_EXECUTABLE" ]; then
+  echo "Visual Studio Code executable not found."
+  exit 1
+fi
+
+# Install GitHub Copilot
+$VSCODE_EXECUTABLE --install-extension github.copilot
