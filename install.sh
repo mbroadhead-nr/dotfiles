649c3b06eed1b0f5694710db7aef54f43d677ef4
Matthew Broadhead <matthew.broadhead@netreveal.ai>
2023-10-19 10:00:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Manual install of code extensions"
+sleep 5
+
 export VSCODE_IPC_HOOK_CLI="$(ls /tmp/vscode-ipc-*.sock  | head -n 1)"
 code_server_bin=$(ps -aux | grep "bin/code-server" | awk '{print $12}' | head -n 1)
 code_bin=$(dirname "$code_server_bin")/remote-cli/code
