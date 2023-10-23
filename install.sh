f718749e4e2fc260e664c8ff5d20c6a73c51c308
Matthew Broadhead <matthew.broadhead@netreveal.ai>
2023-10-23 14:52:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,16 @@ echo $code_bin
 echo "$code_bin --install-extension github.copilot"
 $code_bin --install-extension github.copilot
 
+# echo "Install and startup Tailscale"
+# # sudo apt install tailscale -y
+# curl -fsSL https://tailscale.com/install.sh | sh
+# sudo tailscaled --tun=userspace-networking &
+
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
+
+
