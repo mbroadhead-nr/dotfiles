7eb30acf62de0ffab5172af132cc9afdaf342a43
Matthew Broadhead <matthew.broadhead@netreveal.ai>
2023-10-25 17:28:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,15 +18,23 @@ echo "$code_bin --install-extension github.copilot"
 $code_bin --install-extension github.copilot
 
 # echo "Install and startup Tailscale"
-# # sudo apt install tailscale -y
-# curl -fsSL https://tailscale.com/install.sh | sh
-# sudo tailscaled --tun=userspace-networking &
+curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
+sudo apt-get install proxychains
+sudo truncate -s 0 /etc/proxychains.conf
+sudo tee -a /etc/proxychains.conf > /dev/null <<EOT
+strict_chain
+tcp_read_time_out 15000
+tcp_connect_time_out 8000
+socks5          127.0.0.1       1055
+EOT
 
-type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+# echo "Install Github CLI"
+# type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+# curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+# && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+# && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+# && sudo apt update \
+# && sudo apt install gh -y
 
 
