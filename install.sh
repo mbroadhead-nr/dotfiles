ca83fe55c3baae540732c41933581a38dfc10b04
Matthew Broadhead <matthew.broadhead@netreveal.ai>
2023-11-09 12:57:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-echo "Manual install of code extensions"
-# sleep 10
-
 echo `whoami`
 echo `pwd`
 echo $PATH
-
-export VSCODE_IPC_HOOK_CLI="$(ls /tmp/vscode-ipc-*.sock  | head -n 1)"
-code_server_bin=$(ps -aux | grep "bin/code-server" | awk '{print $12}' | head -n 1)
-code_bin=$(dirname "$code_server_bin")/remote-cli/code
-
-echo $VSCODE_IPC_HOOK_CLI
-echo $code_bin
-
-echo "$code_bin --install-extension github.copilot"
-$code_bin --install-extension github.copilot
-
 echo `git remote get-url origin`
+
+# echo "Manual install of code extensions"
+# # sleep 10
+
+# export VSCODE_IPC_HOOK_CLI="$(ls /tmp/vscode-ipc-*.sock  | head -n 1)"
+# code_server_bin=$(ps -aux | grep "bin/code-server" | awk '{print $12}' | head -n 1)
+# code_bin=$(dirname "$code_server_bin")/remote-cli/code
+
+# echo $VSCODE_IPC_HOOK_CLI
+# echo $code_bin
+
+# echo "$code_bin --install-extension github.copilot"
+# $code_bin --install-extension github.copilot
+
 
 echo "Install and startup Tailscale"
 curl -fsSL https://tailscale.com/install.sh | sh
